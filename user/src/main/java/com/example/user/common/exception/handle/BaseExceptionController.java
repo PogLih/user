910c051bf6cb42f57972e28f5b9d8a7d087c6b57
HCM00034\bp_NamePhong <Phong11.Nguyen@nttdata.com>
2024-07-02 17:11:28 +0700

    upd ExceptionController

--- user/src/main/java/com/example/user/common/exception/handle/BaseExceptionController.java
+++ user/src/main/java/com/example/user/common/exception/handle/BaseExceptionController.java
@@ -1,64 +1,49 @@
 package com.example.user.common.exception.handle;
 
 import com.example.user.common.exception.ApplicationException;
-import com.example.user.common.response.SuccessResponse;
+import com.example.user.common.response.FailureResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
 public class BaseExceptionController extends ResponseEntityExceptionHandler {
-
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleException(Exception e) {
-        // Log the exception (you may want to use a logging framework like SLF4J)
+    public ResponseEntity<?> handleException(Exception e) {
         e.printStackTrace();
-
-        ErrorResponse errorResponse = new ErrorResponse("An internal error occurred");
-
-        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+        Map<String, String> errors = new HashMap<>();
+        errors.put("messages","An internal error occurred");
+        FailureResponse<Object> response = FailureResponse.builder().errors(errors).build();
+        return response.build();
     }
-
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(ApplicationException.class)
-    public ResponseEntity<Object> handleException(ApplicationException ex, WebRequest request) {
-        ResponseData responseData = new ResponseData();
-        responseData.setResult_code(API_ERROR_RESULT_CODE);
-        ValidateErrorObject error = new ValidateErrorObject();
-        error.setErrorCode(
-                !StringUtils.hasLength(ex.getErrorCode()) ? SmpRuleCode.ERR_SYS_001.getLabel() :
-                        ex.getErrorCode());
-        responseData.setValidateError(Arrays.asList(error));
-        return super.handleExceptionInternal(ex, responseData, null, HttpStatus.OK, request);
-        SuccessResponse.builder().build();
-    }
+    public ResponseEntity<?> handleException(ApplicationException ex, WebRequest request) {
+        Map<String, String> errors = new HashMap<>();
+        errors.put("messages","An internal error occurred");
+        FailureResponse<Object> response = FailureResponse.builder().errors(errors).build();
+        return response.build();    }
+
 //    @ResponseStatus(HttpStatus.BAD_REQUEST)
 //    @ExceptionHandler(MethodArgumentNotValidException.class)
-//    public ResponseEntity<?> handleValidationExceptions(MethodArgumentNotValidException ex) {
-//        FailureResponse failureResponse = new FailureResponse();
-//        Map<String, String> errors = new HashMap<>();
-//        ex.getBindingResult().getAllErrors().forEach((error) -> {
-//            String fieldName = ((FieldError) error).getField();
-//            String errorMessage = error.getDefaultMessage();
-//            errors.put(fieldName, errorMessage);
-//        });
-//
-//        failureResponse.setErrors(errors);
-//
-//        return failureResponse.build();
-//    }
-
-    public static class ErrorResponse {
-        private String message;
-
-        public ErrorResponse(String message) {
-            this.message = message;
-        }
-
-        public String getMessage() {
-            return message;
-        }
+    public ResponseEntity<?> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        FailureResponse<Map<String, String>> response = FailureResponse.<Map<String, String>>builder().errors(errors).build();
+        return response.build();
     }
 }
